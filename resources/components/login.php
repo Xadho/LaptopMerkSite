2a0c71498031c516f2bab032efbbfb8716d2c9af
dgg0012 <dgg0012@edu.curio.nl>
2025-04-02 14:28:48 +0200

    Made login.php

---
+++ resources/components/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in</title>
+</head>
+<body>
+    <h1>Log in</h1>
+    <form action="/../../backend/loginController.php" method="post">
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username"><br><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="submit" value="Log in">
+    </form>
+    <p>Don't have an account? <a href="register.php">Register here</a>.</p>
+</body>
+</html>
