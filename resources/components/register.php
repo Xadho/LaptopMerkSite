5069b1887f59f06110335fe6af29c09ec5ffe72a
dgg0012 <dgg0012@edu.curio.nl>
2025-04-02 14:57:45 +0200

    Made changes to register page

--- resources/components/register.php
+++ resources/components/register.php
@@ -6,6 +6,15 @@
     <title>Register</title>
 </head>
 <body>
-    
+    <form action="registerController.php" method="post">
+    <label for="username"></label>
+    <input type="text" id="username" name="username">
+    <label for="password"></label>
+    <input type="password" id="password" name="password" placeholder="Password">
+    <label for="email"></label>
+    <input type="text" id="email" name="email">
+    <input type="submit" value="Register">
+    <p>Already have an account? <a href="login.php">Log in here</a>.</p>
+    </form>
 </body>
 </html>
