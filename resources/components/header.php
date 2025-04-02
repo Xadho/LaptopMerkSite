3d9602266dbe20bd8a3b9a2ca4eba1a52f7f44d3
Xadho <bavo@miezenbeek.com>
2025-04-02 15:00:18 +0200

    Header

---
+++ resources/components/header.php
@@ -0,0 +1,9 @@
+<?php require_once __DIR__.'/../../../config/config.php'; ?>
+
+<header>
+    <div class="container">
+        <div>
+            <a href="#" style="color: lightgrey;">Inloggen</a>
+        </div>
+    </div>
+</header>
